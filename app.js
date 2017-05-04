3db40f2683f3377cf26e2e06284cd09b855c1853
tcreeds <tcreeds@gmail.com>
2017-05-04 19:02:24 -0400

    Added ramda, code for adding subapps is more functional in nature.

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-var express = require('express'),
+var r = require('ramda'),
+    express = require('express'),
     bodyparser = require('body-parser'),
     router = require('./router.js')(express);
 
-var $project_dir = process.env.PS_PROJECT_DIR,
-    app = express(),
+var config = {
+    'projectDir': process.env.PS_PROJECT_DIR,
+    'testapp': {
+        localPath: 'testapp',
+        uri: '/test'
+    }
+}
+
+var app = express(),
     subAppList = [],
     routerDict = {};
 
@@ -11,19 +19,21 @@ app.use('/', router);
 
 router.get('/', express.static('public'));
 
-console.log($project_dir);
+var requireApp = function(name){
+    return require(config.projectDir + '/' + config[name].localPath + '/app.js')(express, config[name]);
+};
 
-function addSubApp(localPath)
-{
-    var subapp = require($project_dir + '/' + localPath + '/app.js')(express);
-    router.use(subapp.uri, subapp.router);
-    console.log('added router to ' + subapp.uri + ' ' + subapp.router);
+function addRoute(app){
+    router.use(app.uri, app.router);
+    return app;
 }
 
 var port = process.env.PORT || 8000;
 app.listen(port);
 console.log("listening on port " + port);
 
-addSubApp('testapp');
+var addApp = r.compose(addRoute, requireApp);
+
+addApp('testapp');
 
 
